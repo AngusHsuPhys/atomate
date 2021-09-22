5a26d0fc45bbb69f4679c2183a05ade2f2bc0e2a
jeng-yuantsai <tsaie79@gmail.com>
2021-09-22 13:24:09 -0400

    update vasptodb

--- atomate/vasp/firetasks/parse_outputs.py
+++ atomate/vasp/firetasks/parse_outputs.py
@@ -108,7 +108,7 @@ class VaspToDb(FiretaskBase):
         # Check for additional keys to set based on the fw_spec
         if self.get("fw_spec_field") and isinstance(self.get("fw_spec_field"), list):
             for key in self.get("fw_spec_field"):
-                task_doc.update(fw_spec[key])
+                task_doc.update({key: fw_spec[key]})
 
         # get the database connection
         db_file = env_chk(self.get('db_file'), fw_spec)
