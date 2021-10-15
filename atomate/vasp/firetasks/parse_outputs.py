2db0e53dd5872255a7248afb473f4f024bceb041
jeng-yuantsai <tsaie79@gmail.com>
2021-10-14 20:53:09 -0400

    update vasptodb

--- atomate/vasp/firetasks/parse_outputs.py
+++ atomate/vasp/firetasks/parse_outputs.py
@@ -109,6 +109,10 @@ class VaspToDb(FiretaskBase):
         if self.get("fw_spec_field") and isinstance(self.get("fw_spec_field"), list):
             for key in self.get("fw_spec_field"):
                 task_doc.update({key: fw_spec[key]})
+        # Automatically add prev fws information
+        for prev_info_key in ["prev_fw_taskid", "prev_fw_db", "prev_fw_collection"]:
+            if prev_info_key in fw_spec:
+                task_doc.update({prev_info_key: fw_spec[prev_info_key]})
 
         # get the database connection
         db_file = env_chk(self.get('db_file'), fw_spec)
@@ -119,14 +123,15 @@ class VaspToDb(FiretaskBase):
                 f.write(json.dumps(task_doc, default=DATETIME_HANDLER))
         else:
             mmdb = VaspCalcDb.from_db_file(db_file, admin=True)
+            # Add current entry information
             task_doc.update({"db": mmdb.db_name, "collection": mmdb.collection.name})
             t_id = mmdb.insert_task(
                 task_doc, use_gridfs=bool(self.get("parse_dos", False))
-                or bool(self.get("bandstructure_mode", False))
-                or bool(self.get("parse_eigenvalues", False))
-                or self.get("parse_chgcar", False)  # deprecated
-                or self.get("parse_aeccar", False)  # deprecated
-                or bool(self.get("store_volumetric_data", STORE_VOLUMETRIC_DATA)))
+                                     or bool(self.get("bandstructure_mode", False))
+                                     or bool(self.get("parse_eigenvalues", False))
+                                     or self.get("parse_chgcar", False)  # deprecated
+                                     or self.get("parse_aeccar", False)  # deprecated
+                                     or bool(self.get("store_volumetric_data", STORE_VOLUMETRIC_DATA)))
             logger.info("Finished parsing with task_id: {}".format(t_id))
 
         defuse_children = False
@@ -146,7 +151,15 @@ class VaspToDb(FiretaskBase):
                 raise RuntimeError("Unknown option for defuse_unsuccessful: "
                                    "{}".format(defuse_unsuccessful))
 
-        task_fields_to_push = self.get("task_fields_to_push", None)
+        task_fields_to_push = self.get("task_fields_to_push", {}) or {}
+        # pass entry information
+        task_fields_to_push.update(
+            {
+                "prev_fw_taskid": "task_id",
+                "prev_fw_db": "db",
+                "prev_fw_collection": "collection"
+            }
+        )
         update_spec = {}
         if task_fields_to_push:
             if isinstance(task_fields_to_push, dict):
