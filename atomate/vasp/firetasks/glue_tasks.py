1bc04c34f7dec8d6bd64edd76d0c55d0887a70b8
jeng-yuantsai <tsaie79@gmail.com>
2021-12-25 03:27:43 -0500

    test

--- atomate/vasp/firetasks/glue_tasks.py
+++ atomate/vasp/firetasks/glue_tasks.py
@@ -95,6 +95,7 @@ class CopyVaspOutputs(CopyFiles):
 
     def copy_files(self):
         all_files = self.fileclient.listdir(self.from_dir)
+        print(all_files)
         # start file copy
         for f in self.files_to_copy:
             prev_path_full = os.path.join(self.from_dir, f)
