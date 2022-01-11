6d75c837f4badf2a8d5225565b583319f8c804f7
tsaie79 <tsaie79@gmail.com>
2022-01-11 13:46:48 -0500

    fix the bug for LobsterRunToDb

--- atomate/vasp/fireworks/lobster.py
+++ atomate/vasp/fireworks/lobster.py
@@ -167,6 +167,7 @@ class LobsterFW(Firework):
         t.append(
             LobsterRunToDb(
                 db_file=db_file,
+                calc_dir=calc_dir,
                 calc_loc=calc_loc,
                 additional_fields=lobstertodb_kwargs,
                 additional_outputs=additional_outputs,
