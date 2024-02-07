fd2d7de5d8ff73c4a3e06601b603ea552aba8a6e
tsaie79 <tsaie79@gmail.com>
2024-02-07 19:51:17 +0000

    Update output file name in run_calc.py

--- atomate/openmx/firetasks/run_calc.py
+++ atomate/openmx/firetasks/run_calc.py
@@ -58,7 +58,7 @@ class RunOpenmx(FiretaskBase):
 
     def run_task(self, fw_spec):
         cmd = env_chk(self["openmx_cmd"], fw_spec)
-        cmd += f" {self.get('input_file', 'input.dat')} > {self.get('output_file', 'output.dat')}"
+        cmd += f" {self.get('input_file', 'input.dat')} > {self.get('output_file', 'output.myout')}"
 
         logger.info(f"Running command: {cmd}")
         return_code = subprocess.call(cmd, shell=True)
