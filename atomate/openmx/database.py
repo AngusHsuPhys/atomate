f914b47868ae3540854ebe34805fdd5d06a8039c
tsaie79 <tsaie79@gmail.com>
2024-02-14 14:44:21 +0000

    Remove admin check in reset method

--- atomate/openmx/database.py
+++ atomate/openmx/database.py
@@ -457,9 +457,6 @@ class openmxCalcDb(CalcDb):
 
 
     def reset(self):
-        if self.admin is False:
-            raise ValueError("You must be an admin to reset the database!")
-    
         self.collection.delete_many({})
         self.db.counter.delete_one({"_id": "taskid"})
         self.db.counter.insert_one({"_id": "taskid", "c": 0})
